3bd5f72734487e98413e3fae65575a1ca4243d55
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-02-05 16:31:02 -0500

    sad

--- Labo4Final.py
+++ Labo4Final.py
@@ -71,90 +71,91 @@ while True:
         print("Incorrect account number or password. Please try again.")
         continue
 
-    break
+    if account_num == "0000" and password == "admin":
+        while True:
+            print("Menu admin:")
+            print("1. Voir tous les comptes")
+            print("2. Ajouter un compte")
+            print("3. Effacer un compte")
+            print("4. Quiiter le terminal")
+
+            admin_choix = int(input("Entrer une action à faire: "))
+
+            if admin_choix == 1:
+                for account in database:
+                    print(account)
+            elif admin_choix == 2:
+                n_compte_num = input("Entrer un numéro de compte: ")
+                n_password = input("Entrer un mot de passe: ")
+                n_cheque = int(input("Saisir le solde du compte chèque: "))
+                n_epargne = int(input("Saisir le solde du compte d'épargne: "))
+                n_investissement = int(input("Saisissez le solde du compte de placement: "))
+                n_interet = float(input("Entrez le taux d'intérêt: "))
+                n_compte = [n_compte_num, n_password, n_cheque, n_epargne, n_investissement, n_interet]
+                database.append(n_compte)
+                print("Nouveau compte ajouté")
+            elif admin_choix == 3:
+                effacer_compte_num = input("Entrer le num du compte à effacer: ")
+                for account in database:
+                    if account[0] == effacer_compte_num:
+                        database.remove(account)
+                        print("Compte effacer")
+                        break
+                else:
+                    print("Compte inexistant")
+            elif admin_choix == 4:
+                break
+            else:
+                print("Choix invalide,. réessayer.")
+
 # Step 2: Choose Account
-print("Choisir un compte suivant:")
-print("1) Cheque")
-print("2) Épargne")
-print("3) Placement")
+    print("Choisir un compte suivant:")
+    print("1) Cheque")
+    print("2) Épargne")
+    print("3) Placement")
 
-choix_utilisateur = int(input("Entrer un choix des comptes: "))
-type_compte = ["Cheque", "Épargne", "Placement"]
+    choix_utilisateur = int(input("Entrer un choix des comptes: "))
+    type_compte = ["Cheque", "Épargne", "Placement"]
 
-# Step 3: Choose Operation
-while True:
-    print("Choisir une action à faire:")
-    print("1) Faire un dépot")
-    print("2) Faire un retrait")
-    if choix_utilisateur == 3:
-        print("3) Voir le retour d'investissement")
-    print("4) Fermer le compte")
-    
-    choix_utilisateur1 = int(input("Entrer un choix des actions à faire: "))
-    current_account = database[choix_utilisateur - 1] 
+    # Step 3: Choose Operation
+    while True:
+        print("Choisir une action à faire:")
+        print("1) Faire un dépot")
+        print("2) Faire un retrait")
+        if choix_utilisateur == 3:
+            print("3) Voir le retour d'investissement")
+        print("4) Fermer le compte")
+        
+        choix_utilisateur1 = int(input("Entrer un choix des actions à faire: "))
+        current_account = database[choix_utilisateur - 1] 
 
-    if choix_utilisateur1 == 1:
-        deposit_amount = int(input("Entrer un dépot à mettre: "))
-        current_account[2] += deposit_amount
-        print(f"Votre {type_compte[choix_utilisateur - 1]} compte a ${current_account[2]}")
-
-    elif choix_utilisateur1 == 2:
-        withdrawal_amount = int(input("Entrer le retrait à faire: "))
-        if current_account[2] - withdrawal_amount < 0:
-            print(f"Pas asser d'argent dans le compte{type_compte[choix_utilisateur - 1]}.")
-        else:
-            current_account[2] -= withdrawal_amount
+        if choix_utilisateur1 == 1:
+            deposit_amount = int(input("Entrer un dépot à mettre: "))
+            current_account[2] += deposit_amount
             print(f"Votre {type_compte[choix_utilisateur - 1]} compte a ${current_account[2]}")
 
-    elif choix_utilisateur1 == 3 and choix_utilisateur == 3:
-        print(f"Votre investissement est ${current_account[choix_utilisateur]}")
-        interest_rate = current_account[5] / 100
-        print(f"Votre interet est {interest_rate}")
-        print("La projection du placement pour les prochains 5 ans est: ")
-        for i in range(1, 6):
-            interest = current_account[choix_utilisateur] * interest_rate
-            current_account[choix_utilisateur] += interest
-            print(f"{i} ans: ${current_account[choix_utilisateur]}")
-
-    elif choix_utilisateur1 == 4:
-        break
-    else:
-        print("Choix invalide, réessayer.")
-        
-# Step 7: Admin Menu
-if account_num == "0000" and password == "admin":
-    while True:
-        print("Menu admin:")
-        print("1. Voir tous les comptes")
-        print("2. Ajouter un compte")
-        print("3. Effacer un compte")
-        print("4. Quiiter le terminal")
-
-        admin_choix = int(input("Entrer une action à faire: "))
-
-        if admin_choix == 1:
-            for account in database:
-                print(account)
-        elif admin_choix == 2:
-            n_compte_num = input("Entrer un numéro de compte: ")
-            n_password = input("Entrer un mot de passe: ")
-            n_cheque = int(input("Saisir le solde du compte chèque: "))
-            n_epargne = int(input("Saisir le solde du compte d'épargne: "))
-            n_investissement = int(input("Saisissez le solde du compte de placement: "))
-            n_interet = float(input("Entrez le taux d'intérêt: "))
-            n_compte = [n_compte_num, n_password, n_cheque, n_epargne, n_investissement, n_interet]
-            database.append(n_compte)
-            print("Nouveau compte ajouté")
-        elif admin_choix == 3:
-            effacer_compte_num = input("Entrer le num du compte à effacer: ")
-            for account in database:
-                if account[0] == effacer_compte_num:
-                    database.remove(account)
-                    print("Compte effacer")
-                    break
+        elif choix_utilisateur1 == 2:
+            withdrawal_amount = int(input("Entrer le retrait à faire: "))
+            if current_account[2] - withdrawal_amount < 0:
+                print(f"Pas asser d'argent dans le compte{type_compte[choix_utilisateur - 1]}.")
             else:
-                print("Compte inexistant")
-        elif admin_choix == 4:
+                current_account[2] -= withdrawal_amount
+                print(f"Votre {type_compte[choix_utilisateur - 1]} compte a ${current_account[2]}")
+
+        elif choix_utilisateur1 == 3 and choix_utilisateur == 3:
+            print(f"Votre investissement est ${current_account[choix_utilisateur]}")
+            interest_rate = current_account[5] / 100
+            print(f"Votre interet est {interest_rate}")
+            print("La projection du placement pour les prochains 5 ans est: ")
+            for i in range(1, 6):
+                interest = current_account[choix_utilisateur] * interest_rate
+                current_account[choix_utilisateur] += interest
+                print(f"{i} ans: ${current_account[choix_utilisateur]}")
+
+        elif choix_utilisateur1 == 4:
             break
         else:
-            print("Choix invalide,. réessayer.")
+            print("Choix invalide, réessayer.")
+            
+    # Step 7: Admin Menu
+
